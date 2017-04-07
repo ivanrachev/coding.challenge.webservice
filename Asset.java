e99e2a43e850e8f6736541351f0bbb5d8ce5aad3
ivanrachev <kadraviat@yahoo.com>
2017-04-08 02:51:03 +0800

    Create Asset.java

---
+++ Asset.java
@@ -0,0 +1,9 @@
+public class Asset {
+
+	/* Asset international identifier */
+	public String ISIN;
+	
+	/* represents the number of shares held in the given asset, i.e. 4 shares of AAPL */
+	public BigDecimal units;
+	
+}
